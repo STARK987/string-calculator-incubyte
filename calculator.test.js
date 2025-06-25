e23891a21134d109cecdbe92d8b03777ad4faf8c
Ashish <ashishkumar@gomechanic.in>
2025-06-25 12:39:28 +0530

    added multiple test cases for TDD

--- calculator.test.js
+++ calculator.test.js
@@ -24,10 +24,27 @@ test('supports custom delimiter', () => {
     expect(add("//;\n1;2")).toBe(3);
 });
 
-test('throws on negative numbers', () => {
-    expect(() => add("1,-2")).toThrow("negative numbers not allowed: -2");
+test('ignores numbers greater than 1000', () => {
+    expect(add("2,1001")).toBe(2);
+    expect(add("1000,1")).toBe(1001);
 });
 
-test('throws with all negative numbers listed', () => {
-    expect(() => add("-1,-2")).toThrow("negative numbers not allowed: -1, -2");
+test('supports multi-character custom delimiters', () => {
+    expect(add("//[***]\n1***2***3")).toBe(6);
+});
+
+test('supports multiple custom delimiters', () => {
+    expect(add("//[*][%]\n1*2%3")).toBe(6);
+});
+
+test('supports multiple multi-character delimiters', () => {
+    expect(add("//[***][%%%]\n1***2%%%3")).toBe(6);
+});
+
+test('throws on a single negative number', () => {
+    expect(() => add("4,-5")).toThrow("negative numbers not allowed: -5");
+});
+
+test('throws with multiple negative numbers', () => {
+    expect(() => add("-1,2,-3")).toThrow("negative numbers not allowed: -1, -3");
 });
